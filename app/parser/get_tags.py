9e81b35229b7ac663033513a623325f57eb5985a
Vitaly Bukin <71148628+rogerezalgo@users.noreply.github.com>
2023-01-28 07:13:38 -0800

    added get_tags function and tests (#4)
    Signed-off-by: Vitaly Bukin <vitivb741@gmail.com>

---
+++ app/parser/get_tags.py
@@ -0,0 +1,21 @@
+import requests as req
+from bs4 import BeautifulSoup
+
+from .check_url_is_available import check_url_is_available
+from .normalize_url import normalize_url
+
+
+def get_tags(url: str):
+    """
+        Parses url from parameter and returns a list of tags
+    """
+
+    is_url_valid = check_url_is_available(url)
+
+    if not is_url_valid:
+        raise ValueError('Can not parse the url')
+
+    resp = req.get(normalize_url(url))
+    soup = BeautifulSoup(resp.text, 'html')
+
+    return [(lambda tag: tag.name)(tag) for tag in soup.recursiveChildGenerator() if tag.name]
